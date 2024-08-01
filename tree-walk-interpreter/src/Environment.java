b87d7890c79f62457676419622d27f289c42a2c1
Optics-Boss <->
2024-08-01 22:41:28 +0200

    Finished variables

--- tree-walk-interpreter/src/Environment.java
+++ tree-walk-interpreter/src/Environment.java
@@ -3,6 +3,15 @@ import java.util.Map;
 
 public class Environment {
     private final Map<String, Object> values = new HashMap<>();
+    final Environment enclosing;
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     public Object get(Token name) {
 
@@ -10,12 +19,19 @@ public class Environment {
             return values.get(name.lexeme);
         }
 
+        if(enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
     public void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        if(enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
