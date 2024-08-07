b2dea05d86cbef8d71f7b402a7ca418fdce114a3
Optics-Boss <->
2024-08-07 22:46:32 +0200

    Finished the Interpreter

--- tree-walk-interpreter/src/Resolver.java
+++ tree-walk-interpreter/src/Resolver.java
@@ -10,7 +10,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 
     private ClassType currentClass = ClassType.NONE;
@@ -36,6 +37,20 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         declare(stmt.name);
         define(stmt.name);
 
+        if(stmt.superclass != null && stmt.name.lexeme.equals(stmt.superclass.name.lexeme)) {
+            Main.error(stmt.superclass.name, "A class can't inherit from itself.");
+        }
+
+        if(stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
+            resolve(stmt.superclass);
+        }
+
+        if(stmt.superclass != null) {
+            beginScope();
+            scopes.peek().put("super", true);
+        }
+
         beginScope();
         scopes.peek().put("this", true);
         endScope();
@@ -46,6 +61,10 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
                 declaration = FunctionType.INITIALIZER;
             }
             resolveFunction(method, declaration);
+        }
+
+        if(stmt.superclass != null) {
+            endScope();
         }
 
         currentClass = enclosingClass;
@@ -178,6 +197,18 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.value);
         resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        if(currentClass == ClassType.NONE) {
+            Main.error(expr.keyword, "Can't use 'super' outside of a class");
+        } else if(currentClass != ClassType.SUBCLASS) {
+            Main.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
+
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
