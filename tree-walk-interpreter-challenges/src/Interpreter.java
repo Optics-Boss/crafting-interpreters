42a002b3cb113d7cc66c18dc069eeda41a6feeed
Optics-Boss <->
2024-12-01 21:30:59 +0100

    Added exception for operands that are 0

--- tree-walk-interpreter-challenges/src/Interpreter.java
+++ tree-walk-interpreter-challenges/src/Interpreter.java
@@ -259,80 +259,57 @@ class Interpreter implements Expr.Visitor<Object>,
 //< Resolving and Binding resolved-assign
     return value;
   }
-//< Statements and State visit-assign
-//> visit-binary
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
     Object right = evaluate(expr.right); // [left]
 
     switch (expr.operator.type) {
-//> binary-equality
       case BANG_EQUAL: return !isEqual(left, right);
       case EQUAL_EQUAL: return isEqual(left, right);
-//< binary-equality
-//> binary-comparison
       case GREATER:
-//> check-greater-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-greater-operand
         return (double)left > (double)right;
       case GREATER_EQUAL:
-//> check-greater-equal-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-greater-equal-operand
         return (double)left >= (double)right;
       case LESS:
-//> check-less-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-less-operand
         return (double)left < (double)right;
       case LESS_EQUAL:
-//> check-less-equal-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-less-equal-operand
         return (double)left <= (double)right;
-//< binary-comparison
       case MINUS:
-//> check-minus-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-minus-operand
         return (double)left - (double)right;
-//> binary-plus
       case PLUS:
         if (left instanceof Double && right instanceof Double) {
           return (double)left + (double)right;
-        } // [plus]
+        }
+
+        if ((left instanceof String && right instanceof Double) ||
+                (left instanceof Double && right instanceof String)) {
+          return left + right.toString();
+        }
 
         if (left instanceof String && right instanceof String) {
           return (String)left + (String)right;
         }
 
-/* Evaluating Expressions binary-plus < Evaluating Expressions string-wrong-type
-        break;
-*/
-//> string-wrong-type
         throw new RuntimeError(expr.operator,
             "Operands must be two numbers or two strings.");
-//< string-wrong-type
-//< binary-plus
       case SLASH:
-//> check-slash-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-slash-operand
         return (double)left / (double)right;
       case STAR:
-//> check-star-operand
         checkNumberOperands(expr.operator, left, right);
-//< check-star-operand
         return (double)left * (double)right;
     }
 
-    // Unreachable.
     return null;
   }
-//< visit-binary
-//> Functions visit-call
+
   @Override
   public Object visitCallExpr(Expr.Call expr) {
     Object callee = evaluate(expr.callee);
@@ -493,16 +470,17 @@ class Interpreter implements Expr.Visitor<Object>,
     if (operand instanceof Double) return;
     throw new RuntimeError(operator, "Operand must be a number.");
   }
-//< check-operand
-//> check-operands
+
   private void checkNumberOperands(Token operator,
                                    Object left, Object right) {
+    if ((Double) left == 0.0 || (Double) right  == 0.0) {
+      throw new RuntimeError(operator, "Operands must not be 0");
+    }
+
     if (left instanceof Double && right instanceof Double) return;
-    // [operand]
     throw new RuntimeError(operator, "Operands must be numbers.");
   }
-//< check-operands
-//> is-truthy
+
   private boolean isTruthy(Object object) {
     if (object == null) return false;
     if (object instanceof Boolean) return (boolean)object;
